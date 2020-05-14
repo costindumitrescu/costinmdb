f161aa85fc716881a5284cda1b2171530749a93d
costindumitrescu <dumitrescucostin20@gmail.com>
2020-05-14 13:37:28 -0400

    finished the project

--- costinmdb/src/App.js
+++ costinmdb/src/App.js
@@ -1,77 +1,81 @@
-import React, {useState} from 'react';
-import Search from './components/Search';
-import Results from './components/Results';
-import Popup from './components/Popup';
-import axios from 'axios';
+import React, { useState } from 'react'
+import axios from 'axios'
 
+import Search from './components/Search'
+import Results from './components/Results'
+import Popup from './components/Popup'
 
 function App() {
-
-const [state, setState] = useState({
-  s: "",
-  results: [],
-  selected: {}
-});
-
-const apiurl= "http://www.omdbapi.com/?i=tt3896198&apikey=a109e64b";
-
-const search = (e) => {
-  if (e.key === "Enter")
-  axios(apiurl + "&s=" + state.s).then(({ data }) => {
-    let results= data.Search;
-
-    setState(prevState => {
-      return {...prevState, results: results }
-    })
-
-  });
-}
-
-
-const handeInput= (e) => {
-  let s = e.target.value;
-
-  setState(prevState => {
-    return {...prevState, s: s}
-  });
-
-
-}
-
-const openPopup = id => {
-  axios(apiurl + "&i=" + id).then(({data}) => {
-    let result = data;
-
-    setState(prevState => {
-      return { ...prevState, selected: result }
+    const [state, setState] = useState({
+        s: "",
+        results: [],
+        selected: {}
     });
-  });
-}
+    const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
 
-const closePopup = () => {
-  setState(prevState => {
-    return {...prevState, selected:{}}
-  });
-}
+    const search = (e) => {
+        if (e.key === "Enter") {
+            axios(apiurl + "&s=" + state.s).then(({ data }) => {
+                let results = data.Search;
 
-    return (
-      <div className = "App" >
-        <header>
-         <h1> Costin Movies DataBase </h1>
-        </header>
-        <main>
-          <Search handleInput={handeInput} search={search}/>
-          <Results results={state.results} openPopup={openPopup} />
+                setState(prevState => {
+                    return {...prevState, results: results }
+                })
+            });
+        }
+    }
 
+    const handleInput = (e) => {
+        let s = e.target.value;
 
-{(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup =
-{closePopup} /> : false}
+        setState(prevState => {
+            return {...prevState, s: s }
+        });
+    }
 
-        </main>
+    const openPopup = id => {
+        axios(apiurl + "&i=" + id).then(({ data }) => {
+            let result = data;
 
-        </div>
+            console.log(result);
 
-          );
-}
+            setState(prevState => {
+                return {...prevState, selected: result }
+            });
+        });
+    }
 
-export default App
+    const closePopup = () => {
+        setState(prevState => {
+            return {...prevState, selected: {} }
+        });
+    }
+
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        h1 > Costin's Movie Database < /h1> <
+        /header> <
+        main >
+        <
+        Search handleInput = { handleInput }
+        search = { search }
+        />
+
+        <
+        Results results = { state.results }
+        openPopup = { openPopup }
+        />
+
+        {
+            (typeof state.selected.Title != "undefined") ? < Popup selected = { state.selected }
+            closePopup = { closePopup }
+            /> : false} <
+            /main> <
+            /div>
+        );
+    }
+
+    export default App
--- costinmdb/src/components/Popup.js
+++ costinmdb/src/components/Popup.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function Popup({selected, closePopup}) {
+function Popup({ selected, closePopup }) {
   return (
-    <section className= "popup">
+    <section className="popup">
       <div className="content">
-        <h2>{selected.Title} <span>({selected.Year})</span></h2>
-        <p className="rating"> Rating: {selected.imdbRating}</p>
+        <h2>{ selected.Title } <span>({ selected.Year })</span></h2>
+        <p className="rating">Rating: {selected.imdbRating}</p>
         <div className="plot">
           <img src={selected.Poster} />
           <p>{selected.Plot}</p>
-       </div>
-          <button className="close" onClick= {closePopup}>Close</button>
-       </div>
+        </div>
+        <button className="close" onClick={closePopup}>Close</button>
+      </div>
     </section>
   )
 }
